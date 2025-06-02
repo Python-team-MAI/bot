72e129081ebe13a3c5795f2583fa73c04ab80b74
Fedor <feodor.a.v.03@gmail.com>
2025-06-02 15:59:27 +0300

    fsdad

--- bot/handlers/on_click/main_menu.py
+++ bot/handlers/on_click/main_menu.py
@@ -37,7 +37,7 @@ async def process_question(message: Message, state: FSMContext, session: AsyncSe
             async with client_session.post(settings.ML_SERVER_URL, headers=headers, json={
                 "message": message.text
                         }) as response:
-                answer = await response.text()
+                answer = await response.post()
         if answer == "invalid token error. Not enough segments":
             access_token, refresh_token = await users_repo.refresh_token(user.refresh_token)
 
@@ -54,7 +54,7 @@ async def process_question(message: Message, state: FSMContext, session: AsyncSe
                     answer = await response.json()
 
         await wait_message.delete()
-        logger.debug(f"Ans: {answer}")
+        logger.debug(f"Ans: {answer}, type: {type(answer)}")
         if answer:
             await message.reply(
                 answer["ans"],
